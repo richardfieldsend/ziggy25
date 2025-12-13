e19eb7504fcf9e8d114032926917200f8bff1d8e
Richard Fieldsend <richardfieldsend@gmail.com>
2025-12-13 22:45:04 +0000

    Modified init.el with automatic updates and created .gitignore.

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -15,3 +15,15 @@
 ;;
 ;; helm.org - Configure helm to filter commands, filenames etc.
 (org-babel-load-file (concat user-emacs-directory "helm.org"))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
