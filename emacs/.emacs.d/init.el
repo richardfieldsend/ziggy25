68d34df19c1f48c7820d0dbc529fe577efa15924
Richard Fieldsend <richardfieldsend@gmail.com>
2025-12-13 22:40:27 +0000

    Initial setup of Emacs configuration with Melpa and Helm setup.

---
+++ emacs/.emacs.d/init.el
@@ -0,0 +1,17 @@
+;; Initialisation File for Emacs 31.0.50
+
+;; The configuration for Emacs is managed through use of use-package
+;; which, in turn 'pulls' the packages from the Melpa repositories and
+;; sets each package up according to configurations included in the
+;; Org files which this file calls.
+;;
+;; In order to help with managing the configuration of Emacs I plan to
+;; use this file and the Org files to compartmentilise packages into
+;; some logical grouping. Comments show what the logic of the grouping
+;; is and the Org files contain appropriate commentary on each setup.
+;;
+;; melpa.org - Configure melpa repository for other packages.
+(org-babel-load-file (concat user-emacs-directory "melpa.org"))
+;;
+;; helm.org - Configure helm to filter commands, filenames etc.
+(org-babel-load-file (concat user-emacs-directory "helm.org"))
