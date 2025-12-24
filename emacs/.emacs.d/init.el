b383c40dd558c98d8fc22a99f7e9b0f35ae91f04
Richard Fieldsend <richardfieldsend@gmail.com>
2025-12-24 00:36:57 +0000

    Configure the eBib setup to manage BibTeX files.

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -60,11 +60,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(auctex badwolf-theme darkroom dired-git-info doom-modeline
-	    emmet-mode fireplace flycheck flyspell-correct-helm focus
-	    magit olivetti org-bullets rainbow-delimiters
-	    selectric-mode writeroom-mode)))
+ '(package-selected-packages nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
